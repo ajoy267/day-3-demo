feda826a7313fe20218ea6b84be0e3831c967035
ajoy267 <andrew.joy12@hotmail.com>
2021-09-29 11:03:57 -0700

    messing around with console.log

--- app.js
+++ app.js
@@ -6,3 +6,27 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+//variables
+const hi = 'history';
+let no = 'nope'; 
+
+//Strings
+console.log (hi + no);
+
+//Boolean
+console.log(true);
+console.log(false);
+
+//numbers
+console.log(1);
+console.log(1 + 1);
+
+//empty-ness
+console.log(null);
+console.log(undefined);
+
+//arrays
+let myList = [2, 3, 5];
+console.log(myList);
+
